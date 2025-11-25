ff66710c273f073e2d9a12d5749525b6033c2a4f
Ankur Chakraborty <ankurchakraborty66@gmail.com>
2025-11-25 04:42:41 +0000

    Day2

---
+++ 25-Nov/basics_list.py
@@ -0,0 +1,17 @@
+nums=[10,20,30,40,50]
+
+print(nums[1:4])
+print(nums[:3])
+print(nums[2:])
+print(nums[::-1])
+
+
+
+print(len(nums))
+print(max(nums))
+print(min(nums))
+
+nums.sort()
+nums.reverse()
+
+print(nums)
