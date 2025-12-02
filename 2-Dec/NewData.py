5d1f5ff453f76b537debface40b9ca2dd744db36
Ankur Chakraborty <ankurchakraborty66@gmail.com>
2025-12-02 10:41:26 +0000

    final

--- 2-Dec/NewData.py
+++ 2-Dec/NewData.py
@@ -119,9 +119,185 @@ df=pd.read_csv("NewData.csv")
 # df["OrderDate"] = pd.to_datetime(df["OrderDate"], errors='coerce')
 # df["ShipDate"] = pd.to_datetime(df["ShipDate"], errors='coerce')
 
-df["ShippingDays"] = (df["ShipDate"] - df["OrderDate"]).dt.days
-mean_shipping_by_category = df.groupby("Category")["ShippingDays"].mean()
-print(mean_shipping_by_category)
+# df["ShippingDays"] = (df["ShipDate"] - df["OrderDate"]).dt.days
+# mean_shipping_by_category = df.groupby("Category")["ShippingDays"].mean()
+# print(mean_shipping_by_category)
 
 
 
+
+#F Set
+
+
+# pivot = pd.pivot_table(
+#     df,
+#     index='Region',
+#     columns='Category',
+#     values='Sales',
+#     aggfunc='sum',
+#     fill_value=0
+# )
+# print(pivot)
+
+# pivot = pd.pivot_table(
+#     df,
+#     index='SubCategory',
+#     columns='Segment',
+#     values='Profit',
+#     aggfunc='sum',
+# )
+#
+# print(pivot)
+
+
+
+# pivot = pd.pivot_table(
+#     df,
+#     index='Region',
+#     columns='Returned',
+#     values='OrderID',
+#     aggfunc='count',
+#     fill_value=0
+# )
+#
+# print(pivot)
+
+
+
+
+# pivot = pd.pivot_table(
+#     df,
+#     index='Category',
+#     values='UnitPrice',
+#     aggfunc='mean'
+# )
+#
+# print(pivot)
+
+
+
+
+# pivot = pd.pivot_table(
+#     df,
+#     index='Month',
+#     columns='Region',
+#     values='Quantity',
+#     aggfunc='sum',
+#     fill_value=0
+# )
+#
+# print(pivot)
+
+
+
+
+#G set
+
+# discount_df = pd.DataFrame({
+#     'Segment': ['Consumer', 'Corporate', 'Home Office'],
+#     'DiscountPct': [5, 8, 10]
+# })
+#
+# df = df.merge(discount_df, on='Segment', how='left')
+# print(df)
+
+
+
+
+# tax_lookup = {
+#     'East': 12,
+#     'West': 10,
+#     'South': 8,
+#     'Central': 9
+# }
+#
+# # Add TaxPct column
+# df['TaxPct'] = df['Region'].map(tax_lookup)
+#
+# print(df)
+
+
+
+# customer_totals = df.groupby('CustomerName').agg({
+#     'Sales': 'sum',
+#     'Profit': 'sum',
+#     'Quantity': 'sum'
+# }).reset_index()
+# customer_totals.rename(columns={
+#     'Sales': 'TotalSales',
+#     'Profit': 'TotalProfit',
+#     'Quantity': 'TotalQuantity'
+# }, inplace=True)
+#
+# df = df.merge(customer_totals, on='CustomerName', how='left')
+# print(df)
+
+
+
+
+
+# product_summary = df.groupby('ProductName').agg({
+#     'Sales': 'sum',
+#     'Profit': 'sum'
+# }).reset_index()
+# product_summary['ProfitMargin'] = (product_summary['Profit'] / product_summary['Sales']).round(2)
+# product_summary.rename(columns={
+#     'Sales': 'TotalSales',
+#     'Profit': 'TotalProfit'
+# }, inplace=True)
+#
+# print(product_summary)
+
+
+
+#H Set
+
+
+# df['OrderDate'] = pd.to_datetime(df['OrderDate'])
+#
+# df['OrderYear'] = df['OrderDate'].dt.year
+# df['OrderMonth'] = df['OrderDate'].dt.month
+# df['OrderDay'] = df['OrderDate'].dt.day
+#
+# print(df)
+
+
+
+
+
+
+
+# df['OrderDate'] = pd.to_datetime(df['OrderDate'])
+# df['OrderDayOfWeek'] = df['OrderDate'].dt.day_name()
+#
+# print(df)
+
+
+
+
+
+
+
+# df['OrderDate'] = pd.to_datetime(df['OrderDate'])
+# df['ShipDate'] = pd.to_datetime(df['ShipDate'])
+#
+# # Calculate ShippingDays
+# df['ShippingDays'] = (df['ShipDate'] - df['OrderDate']).dt.days
+#
+# # Filter orders shipped in more than 5 days
+# delayed_orders = df[df['ShippingDays'] > 5]
+#
+# print(delayed_orders)
+
+
+
+
+
+
+# df['OrderDate'] = pd.to_datetime(df['OrderDate'])
+#
+# df['YearMonth'] = df['OrderDate'].dt.to_period('M')  # e.g., 2025-01, 2025-02
+#
+# monthly_sales = df.groupby('YearMonth')['Sales'].sum().reset_index()
+#
+# print(monthly_sales)
